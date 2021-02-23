94643e0c8d55338bde664f83e84246106587c495
filipecamargos <filipe.atrabalho@gmail.com>
2021-02-23 15:03:08 -0700

    In and ALL used with in subquery

--- Subqueries.sql
+++ Subqueries.sql
@@ -3,14 +3,24 @@ SELECT customer_id, first_name, last_name
 FROM customer
 WHERE customer_id = (SELECT MAX(customer_id) FROM customer);
 
-/**/
+/*Seting a condition to filter the result with a subquery*/
+SELECT city_id, city
+FROM city
+WHERE country_id <> (SELECT country_id FROM country WHERE country = 'India');
 
+/*The example below returns all the cities in the specified countries*/
+SELECT city_id, city
+FROM city
+WHERE country_id IN (SELECT country_id
+					FROM country WHERE country IN ('Canada', 'Mexico'));
 
-/**/
-
-
-/**/
-
+/*Use the all operator to make single comparation in every value in a set
+This query will get all the customer who has not got a free rental*/
+SELECT first_name, last_name
+FROM customer
+WHERE customer_id <> ALL (SELECT customer_id 
+							FROM payment
+							WHERE amount = 0);
 
 /**/
 
