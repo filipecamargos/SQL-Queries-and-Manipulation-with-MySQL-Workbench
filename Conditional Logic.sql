179b2b4af9a92f8f2e31f121d3dd04eb93469a53
filipecamargos <filipe.atrabalho@gmail.com>
2021-03-10 15:38:52 -0700

    Subqueries with case expression

--- Conditional Logic.sql
+++ Conditional Logic.sql
@@ -7,3 +7,26 @@ select first_name, last_name,
         else 'INC'
 	end activity_type
 from customer;
+
+/*Return the rentals for active customers*/
+select c.first_name, c.last_name,
+	case
+		When active = 0 then "N-ACT"
+        else
+			(select count(*) from rental r
+			 where r.customer_id = c.customer_id)
+		end num_rentals
+from customer c;
+
+
+/**/
+
+
+/**/
+
+
+/**/
+
+
+/**/
+
