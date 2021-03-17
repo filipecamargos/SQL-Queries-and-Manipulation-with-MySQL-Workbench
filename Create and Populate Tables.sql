0679dd42651b92d47259ab746ca8c5e0c0dccb5c
filipecamargos <filipe.atrabalho@gmail.com>
2021-03-16 20:19:45 -0600

    Transaction table created

--- Create and Populate Tables.sql
+++ Create and Populate Tables.sql
@@ -1,6 +1,6 @@
 /*Creating a new Table as account table*/
 CREATE TABLE `sakila`.`account` (
-  `account_id` INT NOT NULL,
+  `account_id` INT UNSIGNED NOT NULL,
   `avail_balance` DOUBLE NOT NULL,
   `last_activity_date` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`account_id`));
@@ -14,3 +14,19 @@ VALUES
     
 /*Get the data back to check the values*/
 SELECT * FROM account;
+
+/*Create a transaction table*/
+CREATE TABLE `sakila`.`transaction` (
+  `txn_id` INT UNSIGNED NOT NULL,
+  `txn_date` DATETIME NULL,
+  `account_id` INT NULL,
+  `txn_type_cd` VARCHAR(1) NOT NULL,
+  `amount` DOUBLE NULL,
+  PRIMARY KEY (`txn_id`));
+  
+/*Populate the transaction table*/
+INSERT INTO account
+	(account_id, avail_balance, last_activity_date)
+VALUES
+	(123, 500, now()),
+    (789, 75, now());
