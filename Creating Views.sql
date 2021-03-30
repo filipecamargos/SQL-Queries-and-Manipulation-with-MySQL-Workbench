1dd79d245d4e080ac2f728c15625df3f56ff5840
filipecamargos <filipe.atrabalho@gmail.com>
2021-03-30 14:52:41 -0600

    Views  added

--- Creating Views.sql
+++ Creating Views.sql
@@ -25,3 +25,35 @@ ON fc.category_id = c.category_id
 WHERE c.name = 'Action';
 
 SELECT * from actors_in_action_movies;
+
+/*Creating a film category actor view*/
+CREATE VIEW film_ctgry_actor AS
+SELECT f.title,
+  c.name category_name,
+  a.first_name,
+  a.last_name
+FROM film f
+  INNER JOIN film_category fc
+  ON f.film_id = fc.film_id
+  INNER JOIN category c
+  ON fc.category_id = c.category_id
+  INNER JOIN film_actor fa
+  ON fa.film_id = f.film_id
+  INNER JOIN actor a
+  ON fa.actor_id = a.actor_id;
+  
+/*creating a family movies view*/
+CREATE VIEW family_movies AS
+SELECT  f.title, f.release_year
+FROM film f
+INNER JOIN film_category fc
+ON f.film_id = fc.film_id
+INNER JOIN category c
+ON fc.category_id = c.category_id
+WHERE f.rating IN ('G', 'PG')
+	AND c.name IN ('Animation', 'Children', 'Classics', 'Comedy',
+					'Drama', 'Family')
+ORDER BY f.release_year asc, title;
+
+/*Test View*/
+SELECT * FROM family_movies;
